5dd9f30b92657e71f139c42046f2b75197e146a5
渡辺のぞみ <@10.41.177.117>
2025-03-13 21:54:02 +0900

    オブジェクト指向・コンストラクタ（演習⑤完了）※表示内容を（）内に表記、表示順は下に記載！

--- src/person２/Test.java
+++ src/person２/Test.java
@@ -14,5 +14,17 @@ public class Test {
 		Person jiro = new Person("jiro",20);
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
+		
+		 Person saburo = new Person("saburo");
+		 System.out.println(saburo.name);
+		 System.out.println(saburo.age);
+		 
+		 Person nanashi = new Person(25);
+		 System.out.println(nanashi.name);
+		 System.out.println(nanashi.age);
+		 
+		 Person hanako = new Person(17,"hanako");
+			System.out.println(hanako.name);
+			System.out.println(hanako.age);
 		}
 	}
